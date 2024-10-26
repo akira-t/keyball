a686d351973d8b2e7ca71cb898512baa93439dd0
TANIMOTO Akira <la.vallee.du.livre@gmail.com>
2024-10-26 11:12:06 +0900

    quick tap term を非ゼロ（80）に。hold/tapのtapを連打しなくていいように

--- qmk_firmware/keyboards/keyball/keyball39/keymaps/via/config.h
+++ qmk_firmware/keyboards/keyball/keyball39/keymaps/via/config.h
@@ -40,4 +40,4 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 // 英数ボタンをダブルタップした時に、Layer 3に行ってくれない問題への対応
 //  [\#define QUICK\_TAP\_TERM 0](https://zenn.dev/cou_kamiya/articles/64943a9297ae96)
-#define QUICK_TAP_TERM 0
+#define QUICK_TAP_TERM 80
